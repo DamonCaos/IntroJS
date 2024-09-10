4f7ac1989b5a1138323c578661206742dba459d5
Javier Rodriguez <franciscoromera08162gmail.com>
2024-09-10 22:57:21 +0200

    10/09/24

--- examples/09_destructuring.js
+++ examples/09_destructuring.js
@@ -26,3 +26,63 @@ const [firsBook, secondBook] = [
 
 console.log(firsBook);
 console.log(secondBook);
+
+//const csvProducts = 'T-shirt,10,Amazon\nCap,5,Google\n,Pants,15,Amazon\n,Shoes,20,Google';
+const csvProducts = 'T-shirt,10,Amazon\nCap,5,Google\nPants,15,Amazon\nShoes,20,Google';
+const processCSV = (csv) => {
+    const products = [];
+    const lines = csv.split('\n');
+    for (let line of lines) {
+        const [name, price, store] = line.split(',');
+        products.push({
+            name: name,
+            price: parseInt(price),
+            store: store
+        });
+    }
+return products;
+}
+
+const products = processCSV(csvProducts);
+console.log(products);
+
+
+
+
+
+
+/*const processCSV = (csv) => {
+const products = [];
+  const lines = csv.split('\n'); // ['T-shirt,10,Amazon', 'Cap,5,Google', 'Pants,15,Amazon','Shoes,20,Google']
+for (let line of lines) {
+    // line 'T-shirt,10,Amazon'
+    const [name, price, store] = line.split(','); // ['T-shirt', '10', 'Amazon']
+    products.push({
+    name: name,
+    price: parseInt(price),
+    store: store
+    });
+}
+return products;
+};
+
+const products = processCSV(csvProducts);
+console.log(products);*/
+
+const infoGeneral = {
+    bootcamp: 'KeepCoding'
+};
+
+let module1 = {
+    name: 'JS'
+};
+
+module1 = {...module1, ...infoGeneral };
+console.log(module1)
+
+let module2 = {
+    ...infoGeneral,
+    name: 'HTML'
+};
+
+console.log(module2)
