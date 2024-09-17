50aaffaa4c5a4dd12297a6e987c97b5d01007060
Javier Rodriguez <franciscoromera08162gmail.com>
2024-09-17 21:14:37 +0200

    FEAT: intro async

--- examples/12_async.js
+++ examples/12_async.js
@@ -1,25 +1,99 @@
 console.log('12');
 
-const cargarDatosWeb = () => {
-    setTimeout(() => {
-    datos = [1, 2, 3];
+const cargarDatosWebCb = (cb) => {
+  setTimeout(() => {
+    const datos = [1,2,4];
     cb(datos);
-    }, 1000)
-}
+  }, 1000);
+};
 
-//callbacks
-//promises
-//async/await
+const cargarDatosDetalleCb = (cb) => {
+  setTimeout(() => {
+    const datos = [1,2,4];
+    cb(datos);
+  }, 1000);
+};
 
-console.log('hola');
+const cargarDatosUsuario = () => {
+  setTimeout(() => {
+    const datos = [1,2,4];
+    cb(datos);
+  }, 1000);
+};
 
+const cargarDatosWeb = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const datos = [1,2,4];
+      resolve(datos);
+    }, 1000);
+  });
+};
 
+const cargarDatosDetalle = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const datos = [1,2,4];
+      resolve(datos);
+    }, 1000);
+  });
+};
 
-let loading = true
+const cargarDatosUsuarioCb = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const datos = [1,2,4];
+      resolve(datos);
+    }, 1000);
+  });
+};
 
-cargarDatosWeb((datos) => {
+// callbacks
+// promises | bluebird q
+// async/await
+
+console.log('Hola');
+
+let loading = true;
+
+// callback
+cargarDatosWebCb((datos) => {
   console.log(datos);
-  loading = false
+  cargarDatosDetalleCb(() => {
+    cargarDatosUsuarioCb(() => {
+      loading = false;
+      console.log(loading);
+    });
+  });
 });
+
+// promesas
+cargarDatosWeb()
+  .then(() => cargarDatosDetalle())
+  .then(() => cargarDatosUsuario())
+  .then(() => {
+    loading = false;
+    console.log(loading);
+  })
+  .catch(() => {
+    console.log('Error');
+  });
+
+// async/await
+const main = async () => {
+  try {
+    const datos = await cargarDatosWeb();
+    await cargarDatosDetalle();
+    await cargarDatosUsuario();
+    loading = false;
+    console.log(loading);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+main();
+
+
 
 
